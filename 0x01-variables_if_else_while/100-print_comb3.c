07d4289124334e43a4741dea4e36fca5fe29f63d
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-14 21:31:15 -0700

    combination of 3 numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,23 +10,21 @@ int main(void)
 {
 int i = 0, j, k = 8;
 
-while (i < 8)
+while (i <= 8)
 {
 j = i + 1;
 while (j <= 9)
 {
 putchar(i + '0');
 putchar(j + '0');
+if (i != 8)
 putchar(',');
 putchar(' ');
 j++;
 }
 i++;
 }
-while (k <= 9)
-{
-putchar(k + '0');
-k++;
-}
+putchar('\n');
+
 return (0);
 }
--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,14 +10,15 @@ int main(void)
 {
 int i = 0;
 
-while (i < 9)
+while (i <= 9)
 {
 putchar(i + '0');
+if (i != 9)
 putchar(',');
 putchar(' ');
 i++;
 }
-putchar(i + '0');
+putchar('\n');
 
 return (0);
 }
