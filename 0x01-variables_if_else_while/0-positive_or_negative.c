34cab0dd79d560fb75b1b76787f55984994435aa
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-14 18:03:08 -0700

    determine the sign of the number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-
-
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Entry point
@@ -10,13 +9,15 @@
 
 int main(void)
 {
-int n;
-scanf("%d", &n);
-if (n < 0)
-printf("%d is negative\n", n);
-else if (n == 0)
-printf("%d is zero\n", n);
-else if (n > 0)
-printf("%d is positive\n", n);
-return (0);
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n < 0)
+	printf("%d is negative\n", n);
+	else if (n == 0)
+	printf("%d is zero\n", n);
+	else if (n > 0)
+	printf("%d is positive\n", n);
+	return (0);
 }
