ed8ddcdc15e670b08db49ba3ba21913f4e936067
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-18 13:50:45 -0700

    print the time

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * add - check the sign of the number
+ * @x: is a parameter
+ * @y: is a parameter
+ * Return: Always 0 (Success)
+ */
+
+int add(int x, int y)
+{
+    return (x + y);
+}
+
+
+
+
+
+
+
