2ad5e28d12a50024e1e0567ac0e8bf7cdcf61130
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-18 12:05:02 -0700

    print the last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - check the sign of the number
+ * @n: is a parameter
+ * Return: Always 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+n = n % 10;
+return (n);
+}
+
+
+
+
+
