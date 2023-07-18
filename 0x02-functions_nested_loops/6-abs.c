6925dd66214d78fe66b5252d49a74f22d1d97c30
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-18 12:01:01 -0700

    checks the lower alphbet

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _abs - check the sign of the number
+ * @n: is a parameter
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int n)
+{
+if (n < 0)
+{
+n = -1 * n;
+return (n);
+}
+else
+{
+return (n);
+}
+
+}
+
+
+
+
+
