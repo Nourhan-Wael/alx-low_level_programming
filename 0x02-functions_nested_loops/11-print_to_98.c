9fe5b6d7f97e3993d2645668eb9e1c92138bf00b
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-18 14:37:25 -0700

    print the time

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_to_98 - check the sign of the number
+ * @n: is a parameter
+ * Return: Always 0 (Success)
+ */
+
+void print_to_98(int n)
+{
+int i;
+
+if (n <= 98)
+for (i = n; i < 98; i++)
+
+printf("%d, ", i);
+
+else
+for (i = n; i > 98; i--)
+{
+printf("%d, ", i);
+}
+printf("98");
+}
+
+
+
