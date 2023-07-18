0130f3cac3b9eb5d74b1280021490b9016e72fca
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-18 11:13:18 -0700

    print alphbet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print all the alphabet in small 10 times
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char  i;
+
+int x;
+
+for (x = 0; i < 10; i++)
+for (i = 'a'; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
+
+
+
+
+
+
+
+
+
+
+
