acdc0f08e8ad1bc886dac37acf3ddad1afe29f5c
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-18 13:44:06 -0700

    print the time

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * times_table - check the sign of the number
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+int a, b, n;
+
+for (a = 0; a <= 9; a++)
+{
+_putchar(48);
+for (b = 1; b <= 9; b++)
+{
+_putchar(',');
+_putchar(' ');
+
+n = a * b'
+if (n <= 9)
+_putchar(' ');
+
+else
+_putchar((n / 10) + 48);
+
+_putchar((n % 10) + 48);
+}
+_putchar('\n');
+}
+}
+
+
+
