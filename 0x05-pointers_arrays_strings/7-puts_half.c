3e277773a5027541cfd1d50ffe4fea928ee7fb7d
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-24 19:31:53 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+
+#include "main.h"
+
+/**
+* puts_half - ia a function
+* @str: is a parameter
+* Return: int
+*/
+
+void puts_half(char *str)
+{
+int i, j, z, cn = 0;
+
+for (j = 0; str[j]; j++)
+cn++;
+if (cn % 2 != 0)
+{
+z = ((cn - 1) / 2) + 1;
+for (i = z; i < cn + 1; i++)
+putchar(str[i]);
+}
+else
+for (i = cn / 2; i < cn; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
+}
+
+
+
