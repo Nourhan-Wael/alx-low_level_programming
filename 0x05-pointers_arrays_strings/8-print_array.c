685802e3c92963d6157d80779d3179665c6fda54
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-24 19:58:45 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_array - ia a function
+* @a: is a parameter
+* @n: is a parameter
+* Return: int
+*/
+
+void print_array(int *a, int n)
+{
+int i;
+
+for (i = 0 ; i < n - 1; i++)
+printf("%d, ", a[i]);
+printf("%d", a[i]);
+}
+
+
+
