6cefa1b06e54d39d34d10a0cc3daefa4daab87e7
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-07-24 18:33:37 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* rev_string - ia a function
+* @s: is a parameter
+* Return: int
+*/
+
+void rev_string(char *s)
+{
+int i, j;
+char temp;
+
+for (j = 0; s[j] != '\0'; ++j)
+;
+for (i = 0; i < j / 2; i++)
+{
+temp = s[i];
+s[i] = s[j - 1 - i];
+s[j - 1 - i] = temp;
+}
+
+
+}
+
+
+
