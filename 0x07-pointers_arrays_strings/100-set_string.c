c74059ace8daa7f74d015a68afd388b7e410dbab
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-01 08:46:43 -0700

    edit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* set_string - ia a function
+* @s: is a parameter
+* @to: is a parameter
+* Return: int
+*/
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
+
+
+
+
