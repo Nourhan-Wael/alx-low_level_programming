5b02af929ec57d20ef974499858fa18e9d448029
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-07 18:15:59 -0700

    malloc

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * alloc_grid - Entry point
+ * @width: a parameter
+ * @height: a parameter
+ * Return: Always 0 (Success)
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j, **tab;
+tab = malloc(sizeof(*tab) * height);
+if (width <= 0 || height <= 0 || tab == 0)
+return (NULL);
+
+else
+{
+for (i = 0; i < height; i++)
+{
+tab[i] = malloc(sizeof(**tab) * width);
+if (tab[i] == 0)
+{
+while (i--)
+free(i);
+free(tab);
+return (NULL);
+}
+for (j = 0; j < width; j++)
+tab[i][j] = 0;
+}
+}
+return (tab);
+}
+
+
+
