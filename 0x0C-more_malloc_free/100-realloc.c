78f4263ae414f742c89296cbe2253c9274c332f6
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-17 12:55:08 -0700

    mal;;loc

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _realloc - Entry point
+ * @ptr: a parameter
+ * @old_size: a parameter
+ * @new_size: a parameter
+ * Return: Always 0 (Success)
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+unsigned int i;
+void *p;
+
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
+{
+free(ptr);
+return (NULL);
+}
+if (ptr == NULL)
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+return (p);
+}
+if (new_size > old_size)
+{
+p = malloc(new_size);
+if (p == NULL)
+return (NULL);
+for (i = 0; i < old_size && i < new_size; i++)
+*((char *)p + i) = *((char *)ptr + i);
+free(ptr);
+}
+return (p);
+}
+
+
