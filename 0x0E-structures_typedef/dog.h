fcf45bdb4b0868f1a138b89c8cb4859ed068a39b
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-20 13:06:01 -0700

    struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef structure
+#define structure
+
+/**
+ * struct dog - check the code
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Description: Always 0.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
+
