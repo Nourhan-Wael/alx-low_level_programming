3fb5d688907e79a4b85756e5efd5dffba24bfd3a
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-20 14:19:28 -0700

    struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - check the code
+ * @d: parameter
+ * @name: parameter
+ * @age: parameter
+ * @owner: parameter
+ * Description: Always 0.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+
+
