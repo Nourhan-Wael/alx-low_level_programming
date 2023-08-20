0741dcf059699be8d9f638551afbd824a441081a
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-20 14:52:43 -0700

    struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,35 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - check the code
+ * @d: parameter
+ * Description: Always 0.
+ */
+
+void print_dog(struct dog *d)
+{
+if (d)
+{
+if(d->name == NULL)
+printf("Name: (nil)\n");
+else
+{
+printf("Name: %s\n", d->name);
+}
+if(d->age == 0)
+printf("Age: (nil)\n");
+else
+{
+printf("Age: %f\n", d->age);
+}
+if(d->owner == NULL)
+printf("Owner: (nil)");
+else
+{
+printf("Owner: %s",d->owner);
+}
+}
+
+}
+
+
