604f467017850725c8ef8d647dc481a9d032a51f
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-21 14:52:48 -0700

    function

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,9 +1,11 @@
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * main - Entry point
- * @argc: a parameter
- * @argv: a parameter
- * Return: Always 0 (Success)
+ * main - check the code
+ * @argc: parameter
+ * @argv: parameter
+ * Return: Always 0.
  */
 int main(int argc, char *argv[])
 {
@@ -11,7 +13,7 @@ int sum;
 
 if (argc == 3)
 {
-sum = argv[1] * argv[2];
+sum = atoi(argv[1]) * atoi(argv[2]);
 printf("%d\n", sum);
 }
 else
@@ -19,6 +21,7 @@ else
 printf("Error\n");
 return (1);
 }
+
 return (0);
 }
 
