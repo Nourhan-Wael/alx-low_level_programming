846c2c9d553c228e6aeff677798a0ed4a859effe
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-08-21 09:17:52 -0700

    function

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+/**
+ * print_name - check the code
+ * @name: parameter
+ * @f: parameter
+ * Return: Always 0.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
+
+
