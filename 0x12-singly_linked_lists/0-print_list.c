30bb94dada2c99898861a005236f20b00114b289
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-09-01 14:10:00 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+/**
+ * print_list - prints a list
+ * @h: the integer to print
+ * Return: Nothing.
+ */
+size_t print_list(const list_t *h)
+{
+	size_t cn = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		cn++;
+	}
+	return (cn);
+}
+
+
