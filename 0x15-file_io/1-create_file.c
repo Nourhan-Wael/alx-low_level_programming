b7293fd176550c874c8fcc6003a7f7d1bdfd7907
Nourhan Wael Ahmed Sabbah <nourhanwael12@gmail.com>
2023-09-10 18:02:55 -0700

    file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, 0600 | 0600);
+	fd = open(filename, O_RDONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 	if (len)
@@ -26,5 +26,4 @@ int create_file(const char *filename, char *text_content)
 	close(fd);
 	return (bytes == len ? 1 : -1);
 }
-
 
